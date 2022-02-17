840f45410d2e255fef8ea71e7969446c76de4850
APARNA <aparnashetty2012@gmail.com>
2022-02-17 18:41:59 +0530

    Weather header

---
+++ src/app/weather-header/weather-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-weather-header',
+  templateUrl: './weather-header.component.html',
+  styleUrls: ['./weather-header.component.scss'],
+})
+export class WeatherHeaderComponent {
+  constructor() {}
+}
