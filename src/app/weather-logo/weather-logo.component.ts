b27dd66fbfef128cf32c580b5125cc266ea9f4fe
APARNA <aparnashetty2012@gmail.com>
2022-02-21 14:14:01 +0530

    Weather header

---
+++ src/app/weather-logo/weather-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-weather-logo',
+  templateUrl: './weather-logo.component.html',
+  styleUrls: ['./weather-logo.component.scss'],
+})
+export class WeatherLogoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
