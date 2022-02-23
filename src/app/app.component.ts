daefea9e82881edce727aa502075b26cb4f22d15
APARNA <aparnashetty2012@gmail.com>
2022-02-23 09:43:27 +0530

    weather details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'shoppy'
+  data: any
+  properties = [
+    {
+      iconUrl:
+        '../assets/Images/inspect/weather/Web/01_Home/background/Group 6 Copy/icon_temperature_info.png',
+      text: 'Min-Max',
+    },
+    {
+      iconUrl:
+        '../assets/Images/inspect/weather/Web/01_Home/background/Group 6/icon_precipitation_info.png',
+      text: 'Precipitation',
+    },
+    {
+      iconUrl:
+        '../assets/Images/inspect/weather/Web/01_Home/background/Group 8/icon_humidity_info.png',
+      text: 'Humidity',
+    },
+    {
+      iconUrl:
+        '../assets/Images/inspect/weather/Web/01_Home/background/Group 9/icon_wind_info.png',
+
+      text: 'wind',
+    },
+    {
+      iconUrl:
+        '../assets/Images/inspect/weather/Web/01_Home/background/Group 10/icon_visibility_info.png',
+
+      text: 'Visibility',
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { WeatherDetailsComponent } from './weather-details/weather-details.component'
 import { WeatherHeaderComponent } from './weather-header/weather-header.component'
 import { WeatherLogoComponent } from './weather-logo/weather-logo.component'
 import { WeatherSearchBarComponent } from './weather-search-bar/weather-search-bar.component'
@@ -12,6 +13,7 @@ import { WeatherSearchBarComponent } from './weather-search-bar/weather-search-b
     WeatherHeaderComponent,
     WeatherSearchBarComponent,
     WeatherLogoComponent,
+    WeatherDetailsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
