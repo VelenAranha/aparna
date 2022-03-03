8f222b9785e35c0c4abce41c65af0738f24ce550
APARNA <aparnashetty2012@gmail.com>
2022-03-03 18:04:36 +0530

    wether header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
-import {HttpClientModule}from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { WeatherDetailsComponent } from './weather-details/weather-details.component'
 import { WeatherHeaderComponent } from './weather-header/weather-header.component'
 import { WeatherLogoComponent } from './weather-logo/weather-logo.component'
-import { WeatherSearchBarComponent } from './weather-search-bar/weather-search-bar.component';
-
-
-import { FormsModule } from '@angular/forms'
+import { WeatherSearchBarComponent } from './weather-search-bar/weather-search-bar.component'
 
 @NgModule({
   declarations: [
@@ -18,11 +16,8 @@ import { FormsModule } from '@angular/forms'
     WeatherSearchBarComponent,
     WeatherLogoComponent,
     WeatherDetailsComponent,
-   
-    
-    
   ],
-  imports: [BrowserModule, AppRoutingModule,HttpClientModule,FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/weather-details/weather-details.component.ts
+++ src/app/weather-details/weather-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-weather-details',
@@ -6,38 +6,48 @@ import { Component, Input, OnInit } from '@angular/core'
   styleUrls: ['./weather-details.component.scss'],
 })
 export class WeatherDetailsComponent implements OnInit {
-  
-  WeatherData:any;
+  WeatherData: any
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(){
-    this.WeatherData={
-      main:{},
-      isDay:true
+  ngOnInit() {
+    this.WeatherData = {
+      main: {},
+      isDay: true,
     }
-    this.getWeatherData();
-    console.log(this.WeatherData);
+    this.getWeatherData()
+    console.log(this.WeatherData)
+  }
+  getWeatherData() {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=udupi&appid=cfa27c46b21352fbc56c8e592ffb6eff'
+    )
+      .then((response) => response.json())
+      //let data=JSON.parse('{"coord":{"lon":74.787,"lat":13.352},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"stations","main":{"temp":306.17,"feels_like":312.01,"temp_min":306.17,"temp_max":306.17,"pressure":1009,"humidity":58,"sea_level":1009,"grnd_level":998},"visibility":10000,"wind":{"speed":4.66,"deg":274,"gust":4.42},"clouds":{"all":26},"dt":1645612638,"sys":{"type":1,"id":9217,"country":"IN","sunrise":1645579229,"sunset":1645621703},"timezone":19800,"id":1253952,"name":"Manipal","cod":200}')
+      .then((data: any) => {
+        this.setWeatherData(data)
+      })
+  }
+  setWeatherData(data: any) {
+    this.WeatherData = data
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000)
+    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString()
+    let currentData = new Date()
+    this.WeatherData.isDay = currentData.getTime() < sunsetTime.getTime()
+    this.WeatherData.temp_celcius = (
+      this.WeatherData.main.temp - 273.15
+    ).toFixed(0)
+    this.WeatherData.temp_min = (
+      this.WeatherData.main.temp_min - 273.15
+    ).toFixed(0)
+    this.WeatherData.temp_max = (
+      this.WeatherData.main.temp_max - 273.15
+    ).toFixed(0)
 
-  }
-  getWeatherData(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=udupi&appid=cfa27c46b21352fbc56c8e592ffb6eff').then(response=>response.json())
-    //let data=JSON.parse('{"coord":{"lon":74.787,"lat":13.352},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"stations","main":{"temp":306.17,"feels_like":312.01,"temp_min":306.17,"temp_max":306.17,"pressure":1009,"humidity":58,"sea_level":1009,"grnd_level":998},"visibility":10000,"wind":{"speed":4.66,"deg":274,"gust":4.42},"clouds":{"all":26},"dt":1645612638,"sys":{"type":1,"id":9217,"country":"IN","sunrise":1645579229,"sunset":1645621703},"timezone":19800,"id":1253952,"name":"Manipal","cod":200}')
-    .then((data: any)=>{this.setWeatherData(data);});
-  }
-  setWeatherData(data: any){
-    this.WeatherData=data;
-    let sunsetTime=new Date(this.WeatherData.sys.sunset*1000);
-    this.WeatherData.sunset_time=sunsetTime.toLocaleTimeString();
-    let currentData=new Date();
-    this.WeatherData.isDay=(currentData.getTime()<sunsetTime.getTime());
-    this.WeatherData.temp_celcius=(this.WeatherData.main.temp - 273.15).toFixed(0);
-    this.WeatherData.temp_min=(this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    this.WeatherData.temp_max=(this.WeatherData.main.temp_max - 273.15).toFixed(0);
-   
-    this.WeatherData.temp_wind=(this.WeatherData.wind.speed ).toFixed(0);
-    this.WeatherData.temp_visibility=(this.WeatherData.visibility/1000).toFixed(0);
-  
+    this.WeatherData.temp_wind = this.WeatherData.wind.speed.toFixed(0)
+    this.WeatherData.temp_visibility = (
+      this.WeatherData.visibility / 1000
+    ).toFixed(0)
   }
   data: any
   properties = [
@@ -45,7 +55,6 @@ export class WeatherDetailsComponent implements OnInit {
       iconUrl:
         '../assets/Images/inspect/weather/Web/01_Home/background/Group 6 Copy/icon_temperature_info.png',
       text: 'Min-Max',
-      
     },
     {
       iconUrl:
@@ -70,5 +79,4 @@ export class WeatherDetailsComponent implements OnInit {
       text: 'Visibility',
     },
   ]
-  
 }
